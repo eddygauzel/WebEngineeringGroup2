62c03a11a351af97866320bbb2be945294fd71ef
eddygauzel <eddy-gauzel@hotmail.com>
2016-06-09 17:01:07 +0200

    änderung in dern seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(loginName: "eddy", pubkey_user: "654687464", salt_masterkey: ajdfhldshkf57357, privatekey_user_enc: lajsfhdsjhflkjHF)
+User.create(loginName: "eddy", pubkey_user: "654687464", salt_masterkey: "ajdfhldshkf57357", privatekey_user_enc: "lajsfhdsjhflkjHF")
